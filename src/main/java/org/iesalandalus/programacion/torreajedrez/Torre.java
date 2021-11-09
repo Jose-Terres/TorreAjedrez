dfd467c8d3a3b2d7baa41118b363fb2108b07fc4
pepet <pepet@Portatil>
2021-11-10 00:05:44 +0100

    Ejercicio 11 Creacion clase Torre

---
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -0,0 +1,8 @@
+package org.iesalandalus.programacion.torreajedrez;
+
+// Creamos la clase Torre con los atributos Color y Posicion
+public class Torre {
+	private Color  color;
+	private Posicion posicion;
+
+}
