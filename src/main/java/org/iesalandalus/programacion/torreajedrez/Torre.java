b6b223dde35fad50353446623bd48d22551f24bc
pepet <pepet@Portatil>
2021-11-12 17:04:32 +0100

    Ejercicio 14 constructor para la clase que acepte como parámetro color

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -13,6 +13,20 @@ public class Torre {
 		//Llamamos al método setcolor y luego le metemos como parametro Color.Negro por fedecto.
 		setPosicion(new Posicion(8,'h'));
 	}
+	// creamos constructor por defecto, el nombre siempre es el mismo que el de la clase
+	public Torre( Color color) {
+		//llamamos al constructor al que le metemos la variable que nos han introducido color
+		setColor(color);
+		
+	// comprobamos que la variable color si es igual a Color.NEGRO la posicion tiene que ser 8,h
+		if(color == Color.NEGRO) {
+			setPosicion(new Posicion(8,'h'));
+			// En caso de que el color no sea igual a Color,NEGRO la posicion tiene que ser 8,h
+		}else {
+			setPosicion(new Posicion(1,'h'));	
+		}
+		
+	}
 	
 	public Color getColor() {
 		return color;
