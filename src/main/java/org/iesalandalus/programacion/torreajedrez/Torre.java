c76cb22282830148c5ce56750c6506e823bec3e0
pepet <pepet@Portatil>
2021-11-12 16:52:01 +0100

    Ejercicio 13 Constructor por defercto Torrre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,8 +1,32 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+
 // Creamos la clase Torre con los atributos Color y Posicion
 public class Torre {
 	private Color  color;
 	private Posicion posicion;
+		
+	// creamos constructor por defecto, el nombre siempre es el mismo que el de la clase
+	public Torre() {
+		//Llamamos al método setcolor y luego le metemos como parametro Color.Negro por fedecto.
+		setColor(Color.NEGRO);
+		//Llamamos al método setcolor y luego le metemos como parametro Color.Negro por fedecto.
+		setPosicion(new Posicion(8,'h'));
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 
+	
+	
 }
